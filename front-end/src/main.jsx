1f9bf84d43d69103c130d30d2fb0fb31924c644d
KamilaMatysiak <kamamat99@wp.pl>
2024-06-17 12:53:11 +0200

    add global styles

--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import './styles/theme.css'
+import './styles/global.css'
 
 const router = createBrowserRouter([
   {
