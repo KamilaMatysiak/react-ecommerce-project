0eef0f1feabf88696e08ca63e7c8bcbb6f4d6600
Kamila Matysiak <kmat@corp.coboru.com.pl>
2024-07-08 11:51:56 +0200

    adds ExpandableMenu component to ProductList

--- front-end/src/components/ExpandableMenu/ExpandableMenu.jsx
+++ front-end/src/components/ExpandableMenu/ExpandableMenu.jsx
@@ -1,3 +1,42 @@
+import styles from "./ExpandableMenu.module.css"
+import { CATEGORIES } from "../../constants/categories"
+import { NavLink } from "react-router-dom"
+import ARROW from "../../assets/Icons/arrow.svg"
+
 export const ExpandableMenu = () => {
-  return <div>ExpandableMenu</div>
+  const activePath = "odziez"
+
+  return (
+    <div className={styles.expandableMenu}>
+      <p>Kobieta</p>
+      <ul>
+        {CATEGORIES.map((category) => {
+          return (
+            <li key={category.path}>
+              <NavLink to={category.path}>
+                {category.name}
+                <img
+                  src={ARROW}
+                  className={activePath == category.path ? styles.expanded : ""}
+                />
+              </NavLink>
+              {activePath == category.path && (
+                <ul>
+                  {category.subcategories.map((subcategory) => {
+                    return (
+                      <li key={subcategory.path}>
+                        <NavLink to={subcategory.path}>
+                          {subcategory.name}
+                        </NavLink>
+                      </li>
+                    )
+                  })}
+                </ul>
+              )}
+            </li>
+          )
+        })}
+      </ul>
+    </div>
+  )
 }
