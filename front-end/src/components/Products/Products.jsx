5e2ada7416ff4087cd4deb4277b478fc7f554f53
KamilaMatysiak <kamamat99@wp.pl>
2024-07-08 12:20:08 +0200

    rename Bestsellers component to Products and add to ProductList view

---
+++ front-end/src/components/Products/Products.jsx
@@ -0,0 +1,16 @@
+import { CenteredContent } from "../CenteredContent/CenteredContent"
+import { Product } from "../Product/Product"
+import styles from "./Products.module.css"
+
+export const Products = ({ products, headerText }) => {
+  return (
+    <CenteredContent>
+      <h2 className={styles.bestsellersHeader}>{headerText}</h2>
+      <div className={styles.productsWrapper}>
+        {products.map((product) => (
+          <Product key={product.id} product={product} />
+        ))}
+      </div>
+    </CenteredContent>
+  )
+}
