f431ae995412c1a84ea20bc6840355914aec31a6
KamilaMatysiak <kamamat99@wp.pl>
2024-07-12 10:45:55 +0200

    fix breadcrumbs

--- front-end/src/components/Breadcrumbs/Breadcrumbs.jsx
+++ front-end/src/components/Breadcrumbs/Breadcrumbs.jsx
@@ -1,23 +1,38 @@
 import styles from "./Breadcrumbs.module.css"
 import { NavLink, useParams } from "react-router-dom"
 import ARROW from "../../assets/Icons/arrow.svg"
+import { CATEGORIES, GENDERS } from "../../constants/categories"
 
 export const Breadcrumbs = () => {
-  const params = useParams()
+  const { gender, category, subcategory } = useParams()
+  const foundGender = GENDERS.find((g) => g.path === gender)
+  const foundCategory = CATEGORIES.find((c) => c.path === category)
+
   const breadcrumbs = [
-    { name: params.gender, path: params.gender },
-    { name: params.category, path: `${params.gender}/${params.category}` },
+    { name: foundGender.name, path: foundGender.path },
     {
-      name: params.subcategory,
-      path: `${params.gender}/${params.category}/${params.subcategory}`,
+      name: foundCategory.name,
+      path: `/${foundGender.path}/${foundCategory.path}`,
     },
   ]
+
+  if (subcategory) {
+    const foundSubcategory = foundCategory.subcategories.find(
+      (s) => s.path === subcategory
+    )
+
+    breadcrumbs.push({
+      name: foundSubcategory.name,
+      path: `/${foundGender.path}/${foundCategory.path}/${foundSubcategory.path}`,
+    })
+  }
+
   return (
     <div className={styles.breadcrumbs}>
       {breadcrumbs.map((breadcrumb) => {
         return (
           <li key={breadcrumb.path}>
-            <NavLink to={breadcrumb.path}>
+            <NavLink end to={breadcrumb.path}>
               {breadcrumb.name} <img src={ARROW} />
             </NavLink>
           </li>
